686eac591ab4b7e15eab06a61909a40f59aacd89
Yehuda1223 <103877785+Yehuda1223@users.noreply.github.com>
2022-05-04 18:14:46 +0300

    winer+button+game over

--- src/GameScen.java
+++ src/GameScen.java
@@ -29,7 +29,7 @@ public class GameScen extends JPanel  {
         //obstacles  +image+bug
 
         Random random = new Random();
-        int lower = -800000;
+        int lower = -800001;
         int maxer = 1;
         this.obstacles = new CustomRectangel[4000];
         for (int i = 0; i < this.obstacles.length; i++) {
